f1ec42657a8a45e444a842e47d053c435cea01d7
K0H205 <hrswkhd@gmail.com>
2018-07-22 12:43:18 +0900

    利用規約、問い合わせページ追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
     
   def about
+  end
 
+  def contact
+  end
+
+  def agreement
   end
   
   def login
-
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#login'
   get '/about', to: 'home#about'
+  get '/agreement', to: 'home#agreement'
+  get '/contact', to: 'home#contact'
   get '/auth/twitter/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/search', to: 'books#search'
