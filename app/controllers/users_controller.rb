f83c9f1470684731722814a448d495d05e86dd95
K0H205 <hrswkhd@gmail.com>
2018-07-31 06:29:12 +0900

    showページにページネーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.paginate(page: params[:page], per_page: 10)
   end
 
   def index
